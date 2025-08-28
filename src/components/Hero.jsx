0c7815d43a22a37fe583574aa5b3fd1d3b2525cb
wafi1122 <fstimatr00040@gmail/com>
2025-08-28 14:36:48 +0500

    new commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,25 @@
-const HeroSection = () =>{
-    return(
+const HeroSection = () => {
+    return (
         <main className="hero container">
             <div className="hero-content">
-            <h1>YOUR FEET DESERVE THE BEST</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque accusantium a eligendi ad doloremque iusto incidunt blanditiis rem provident recusandae, nemo harum! Sequi.</p>
+                <h1>YOUR FEET DESERVE THE BEST</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque accusantium a eligendi ad doloremque iusto incidunt blanditiis rem provident recusandae, nemo harum! Sequi.</p>
 
-            <div className="hero-btn">
-                <button>Shop Now</button>
-                <button className="secondary-btn">Catoegory</button>
-            </div>
-            <div className="shopping">
-                <p>Also Aviaiable On</p>
-                <div className="brand-icons">
-                    <img src="/images/amazon.png" alt="" />
-                    <img src="/images/flipkart.png" alt="" />
+                <div className="hero-btn">
+                    <button>Shop Now</button>
+                    <button className="secondary-btn">Catoegory</button>
                 </div>
-            </div>
+                <div className="shopping">
+                    <p>Also Aviaiable On</p>
+                    <div className="brand-icons">
+                        <img src="/images/amazon.png" alt="" />
+                        <img src="/images/flipkart.png" alt="" />
+                    </div>
+                </div>
             </div>
 
             <div className="hero-image">
-                <img src="/images/shoe_image.png" alt="" />    
+                <img src="/images/shoe_image.png"  alt="" />
             </div>
         </main>
     )
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,19 +1,19 @@
-const Navigation = () =>{
-    return(
+const Navigation = () => {
+    return (
         <nav className="container">
-        <div className="logo">
-            <img src="/images/brand_logo.png" alt="Logo" />
-        </div>
-        <ul>
-            <li href = "#">Menu</li>
-            <li href = "#">Location</li>
-            <li href = "#">About</li>
-            <li href = "#">Contact</li>
-        </ul>
-        <button>Login</button>
+            <div className="logo">
+                <img src="/images/brand_logo.png" alt="Logo" />
+            </div>
+            <ul>
+                <li href="#">Menu</li>
+                <li href="#">Location</li>
+                <li href="#">About</li>
+                <li href="#">Contact</li>
+            </ul>
+            <button>Login</button>
 
 
-    </nav>
+        </nav>
     )
 }
 export default Navigation;
